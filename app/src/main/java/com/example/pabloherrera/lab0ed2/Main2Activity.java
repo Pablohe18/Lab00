e92f4c69682ed3def22644a29f86dd5e9a0d7af8
Pablohe18 <carlospablo12340@hotmail.com>
2018-09-05 17:16:59 -0600

    0.3

--- app/src/main/java/com/example/pabloherrera/lab0ed2/Main2Activity.java
+++ app/src/main/java/com/example/pabloherrera/lab0ed2/Main2Activity.java
@@ -1,42 +1,179 @@
 package com.example.pabloherrera.lab0ed2;
 
-import android.graphics.Color;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Main2Activity extends AppCompatActivity {
 
 
+    private ArrayList<String> canciones;
     private ListView lista;
-    List<String> items;
-    ArrayAdapter ADP;
+    private EditText et_buscador;
+    private EditText txtAgregar;
+    private Button btnAgregar;
+    ArrayAdapter<String> adapter;
+    int counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
-
-        final EditText txtNombre = findViewById(R.id.txtNombre);
-        final Button btnAgregar = findViewById(R.id.btnAgregar);
-        lista = findViewById(R.id.lista);
-        items = new ArrayList<>();
-        ADP = new ArrayAdapter(getApplicationContext(),android.R.layout.simple_list_item_1,items);
-        lista.setAdapter(ADP);
-
-        lista.setBackgroundColor(Color.DKGRAY);
-        btnAgregar.setOnClickListener(new View.OnClickListener() {
+        lista = (ListView) findViewById(R.id.lista);
+        if (counter == 0){
+            canciones = new ArrayList<String>();
+            canciones.add("Noche Complicada; 3:45");
+            canciones.add("Chica paranormal ; 3:41");
+            canciones.add("BEBE ; 3:37");
+            canciones.add("CaraLuna ; 4:20");
+            canciones.add("Maquiavelico ; 3:25");
+            canciones.add("Feliz LSD ; 3:87");
+            canciones.add("Chica loca ; 4:41");
+            canciones.add("Feliz Navidad.mp3 ; 3:37");
+            canciones.add("DimitriVegas ; 7:20");
+            canciones.add("Juanes ; 3:28");
+            canciones.add("materia Complicada ; 5:32");
+            canciones.add("Actividad paranormal ; 8:41");
+            canciones.add("BUBU ; 2:37");
+            canciones.add("Mani con rocs ; 4:20");
+            canciones.add("Tu ; 1:25");
+        }
+            counter++;
+        et_buscador = (EditText) findViewById(R.id.et_buscar);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, canciones);
+        lista.setAdapter(adapter);
+        et_buscador.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View view) {
-                items.add(txtNombre.getText().toString());
-                ADP.notifyDataSetChanged();
+            public void beforeTextChanged(CharSequence s, int i, int i1, int i2) {
+
+                lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        final int posicion1 = i;
+
+                        AlertDialog.Builder dialogo1 = new AlertDialog.Builder(Main2Activity.this);
+                        dialogo1.setTitle("Importante");
+                        dialogo1.setMessage("¿ Elimina este teléfono ?");
+                        dialogo1.setCancelable(false);
+                        dialogo1.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialogo1, int id) {
+                                canciones.remove(posicion1);
+                                adapter.notifyDataSetChanged();
+                            }
+                        });
+                        dialogo1.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialogo1, int id) {
+                            }
+                        });
+                        dialogo1.show();
+
+                        return false;
+                    }
+                });
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int i, int i1, int i2) {
+                adapter.getFilter().filter(s);
+
+                lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        final int posicion1 = i;
+
+                        AlertDialog.Builder dialogo1 = new AlertDialog.Builder(Main2Activity.this);
+                        dialogo1.setTitle("Importante");
+                        dialogo1.setMessage("¿ Elimina este teléfono ?");
+                        dialogo1.setCancelable(false);
+                        dialogo1.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialogo1, int id) {
+                                canciones.remove(posicion1);
+                                adapter.notifyDataSetChanged();
+                            }
+                        });
+                        dialogo1.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialogo1, int id) {
+                            }
+                        });
+                        dialogo1.show();
+
+                        return false;
+                    }
+                });
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        final int posicion1 = i;
+
+                        AlertDialog.Builder dialogo1 = new AlertDialog.Builder(Main2Activity.this);
+                        dialogo1.setTitle("Importante");
+                        dialogo1.setMessage("¿ Elimina este teléfono ?");
+                        dialogo1.setCancelable(false);
+                        dialogo1.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialogo1, int id) {
+                                canciones.remove(posicion1);
+                                adapter.notifyDataSetChanged();
+                            }
+                        });
+                        dialogo1.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialogo1, int id) {
+                            }
+                        });
+                        dialogo1.show();
+
+                        return false;
+                    }
+                });
+
+            }
+        });
+
+        lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                final int posicion = i;
+
+                AlertDialog.Builder dialogo1 = new AlertDialog.Builder(Main2Activity.this);
+                dialogo1.setTitle("Importante");
+                dialogo1.setMessage("¿Elimina esta cancion?");
+                dialogo1.setCancelable(false);
+                dialogo1.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogo1, int id) {
+                        canciones.remove(posicion);
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+                dialogo1.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogo1, int id) {
+                    }
+                });
+                dialogo1.show();
+
+                return false;
             }
         });
     }
+
+
+    public void agregar(View v) {
+        canciones.add(txtAgregar.getText().toString());
+        adapter.notifyDataSetChanged();
+        txtAgregar.setText("");
+    }
+
 }
