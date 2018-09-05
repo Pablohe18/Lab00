bed903164c22e5e086074e401f39b7ca61f1cbd5
Pablohe18 <carlospablo12340@hotmail.com>
2018-09-04 20:15:18 -0600

    asd
    asdaas

--- app/src/main/java/com/example/pabloherrera/lab0ed2/Main2Activity.java
+++ app/src/main/java/com/example/pabloherrera/lab0ed2/Main2Activity.java
@@ -1,15 +1,11 @@
 package com.example.pabloherrera.lab0ed2;
 
 import android.annotation.SuppressLint;
-import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
-
 
 import java.util.ArrayList;
 import java.util.List;
