373fe85c9a0397e4171fc9feb628d8c9cd334106
Pablohe18 <carlospablo12340@hotmail.com>
2018-09-04 22:14:22 -0600

    0.1

--- app/src/main/java/com/example/pabloherrera/lab0ed2/Main2Activity.java
+++ app/src/main/java/com/example/pabloherrera/lab0ed2/Main2Activity.java
@@ -1,6 +1,5 @@
 package com.example.pabloherrera.lab0ed2;
 
-import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,12 +8,9 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static android.R.layout.simple_list_item_1;
 
 public class Main2Activity extends AppCompatActivity {
 
--- app/src/main/java/com/example/pabloherrera/lab0ed2/MainActivity.java
+++ app/src/main/java/com/example/pabloherrera/lab0ed2/MainActivity.java
@@ -14,14 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        button = (Button) findViewById(R.id.button);
+        button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 Intent button = new Intent(MainActivity.this, Main2Activity.class);
                 startActivity(button);
-
             }
         });
     }
