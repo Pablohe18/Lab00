c45e02fb192d3710b34278ad20eb12ee1e15283a
Pablohe18 <carlospablo12340@hotmail.com>
2018-09-04 20:42:28 -0600

    0.1

--- app/src/main/java/com/example/pabloherrera/lab0ed2/Main2Activity.java
+++ app/src/main/java/com/example/pabloherrera/lab0ed2/Main2Activity.java
@@ -3,34 +3,41 @@ package com.example.pabloherrera.lab0ed2;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.R.layout.simple_list_item_1;
+
 public class Main2Activity extends AppCompatActivity {
-    private ArrayList<String> Lista;
+    private EditText Micaja;
+    private ListView Misalida;
+    private Button BotonMostrar;
     private ArrayAdapter<String> adaptador1;
-    private ListView lv1;
-    private EditText et1;
-    
-    List<String> lista = new ArrayList<>();
+    private ArrayList<String> lista;
+
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
-        lista= new ArrayList<>();
-        adaptador1= new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, lista);
-        lv1= findViewById(R.id.Listashow);
-        lv1.setAdapter(adaptador1);
+        lista=new ArrayList<String>();
+        Micaja = (EditText) findViewById(R.id.txtNombre);
+        Misalida = (ListView) findViewById(R.id.txtSalida);
+        BotonMostrar = (Button) findViewById(R.id.btnNombre);
 
-        et1= findViewById(R.id.button);
-
-
+        BotonMostrar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                
+            }
         
     }
 
